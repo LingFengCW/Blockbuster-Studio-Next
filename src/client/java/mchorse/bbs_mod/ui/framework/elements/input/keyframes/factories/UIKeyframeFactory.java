0cc34d742009718daa4558421d3991d749bdb443
lemonungood <lemonungood@users.noreply.github.com>
2026-09-12 14:42:58 +0800

    关键帧面板：贝塞尔切线手柄 LX/LY/RX/RY 数值精编

--- src/client/java/mchorse/bbs_mod/ui/framework/elements/input/keyframes/factories/UIKeyframeFactory.java
+++ src/client/java/mchorse/bbs_mod/ui/framework/elements/input/keyframes/factories/UIKeyframeFactory.java
@@ -6,6 +6,7 @@ import mchorse.bbs_mod.ui.UIKeys;
 import mchorse.bbs_mod.ui.framework.elements.UIElement;
 import mchorse.bbs_mod.ui.framework.elements.UIScrollView;
 import mchorse.bbs_mod.ui.framework.elements.buttons.UIIcon;
+import mchorse.bbs_mod.ui.framework.elements.utils.UILabel;
 import mchorse.bbs_mod.ui.framework.elements.context.UIInterpolationContextMenu;
 import mchorse.bbs_mod.ui.framework.elements.events.UITrackpadDragEndEvent;
 import mchorse.bbs_mod.ui.framework.elements.events.UITrackpadDragStartEvent;
@@ -16,10 +17,12 @@ import mchorse.bbs_mod.ui.framework.elements.input.keyframes.UIKeyframes;
 import mchorse.bbs_mod.ui.framework.elements.input.keyframes.shapes.IKeyframeShapeRenderer;
 import mchorse.bbs_mod.ui.framework.elements.input.keyframes.shapes.KeyframeShapeRenderers;
 import mchorse.bbs_mod.ui.framework.tooltips.InterpolationTooltip;
+import mchorse.bbs_mod.l10n.keys.IKey;
 import mchorse.bbs_mod.ui.utils.UI;
 import mchorse.bbs_mod.ui.utils.icons.Icons;
 import mchorse.bbs_mod.utils.colors.Color;
 import mchorse.bbs_mod.utils.interps.Interpolation;
+import mchorse.bbs_mod.utils.interps.Interpolations;
 import mchorse.bbs_mod.utils.keyframes.Keyframe;
 import mchorse.bbs_mod.utils.keyframes.KeyframeShape;
 import mchorse.bbs_mod.utils.keyframes.factories.IKeyframeFactory;
@@ -27,6 +30,7 @@ import mchorse.bbs_mod.utils.keyframes.factories.KeyframeFactories;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Consumer;
 
 public abstract class UIKeyframeFactory <T> extends UIElement
 {
@@ -40,6 +44,18 @@ public abstract class UIKeyframeFactory <T> extends UIElement
 
     public UIIcon shape;
     public UIColor color;
+
+    /* Bezier tangent handle numeric editors (only relevant for BEZIER interpolation) */
+    public UITrackpad lxPad;
+    public UITrackpad lyPad;
+    public UITrackpad rxPad;
+    public UITrackpad ryPad;
+
+    private UIElement lxRow;
+    private UIElement lyRow;
+    private UIElement rxRow;
+    private UIElement ryRow;
+    private boolean bezierHandlesVisible;
 
     protected Keyframe<T> keyframe;
     protected UIKeyframes editor;
@@ -112,7 +128,11 @@ public abstract class UIKeyframeFactory <T> extends UIElement
             Interpolation interp = this.keyframe.getInterpolation();
             UIInterpolationContextMenu menu = new UIInterpolationContextMenu(interp);
 
-            this.getContext().replaceContextMenu(menu.callback(() -> this.editor.getGraph().setInterpolation(interp)));
+            this.getContext().replaceContextMenu(menu.callback(() ->
+            {
+                this.editor.getGraph().setInterpolation(interp);
+                this.syncBezierHandles();
+            }));
         });
         this.interp.tooltip(new InterpolationTooltip(0F, 0.5F, () -> this.keyframe.getInterpolation()));
         this.interp.keys().register(Keys.KEYFRAMES_INTERP, this.interp::clickItself).category(UIKeys.KEYFRAMES_KEYS_CATEGORY);
@@ -167,11 +187,83 @@ public abstract class UIKeyframeFactory <T> extends UIElement
         this.scroll.add(UI.row(this.interp, this.tick, this.duration));
         this.scroll.add(UI.row(this.shape, this.color));
 
+        /* Bezier tangent handle numeric editors (added to the scroll only
+         * when the selected keyframe uses BEZIER interpolation) */
+        this.lxPad = this.createHandlePad((v) -> this.keyframe.lx = (float) v.doubleValue());
+        this.lyPad = this.createHandlePad((v) -> this.keyframe.ly = (float) v.doubleValue());
+        this.rxPad = this.createHandlePad((v) -> this.keyframe.rx = (float) v.doubleValue());
+        this.ryPad = this.createHandlePad((v) -> this.keyframe.ry = (float) v.doubleValue());
+
+        this.lxRow = this.handleField(IKey.raw("LX"), this.lxPad);
+        this.lyRow = this.handleField(IKey.raw("LY"), this.lyPad);
+        this.rxRow = this.handleField(IKey.raw("RX"), this.rxPad);
+        this.ryRow = this.handleField(IKey.raw("RY"), this.ryPad);
+
         this.add(this.scroll);
 
         /* Fill data */
         this.tick.setValue(TimeUtils.toTime(keyframe.getTick()));
         this.duration.setValue(TimeUtils.toTime(keyframe.getDuration()));
+        this.syncBezierHandles();
+    }
+
+    private UITrackpad createHandlePad(Consumer<Double> callback)
+    {
+        UITrackpad pad = new UITrackpad(callback);
+
+        pad.limit(Float.NEGATIVE_INFINITY, Float.POSITIVE_INFINITY);
+        pad.tooltip(IKey.raw("Bezier tangent handle offset (in ticks / value units)"));
+        pad.getEvents().register(UITrackpadDragStartEvent.class, (e) -> this.editor.cacheKeyframes());
+        pad.getEvents().register(UITrackpadDragEndEvent.class, (e) -> this.editor.submitKeyframes());
+
+        return pad;
+    }
+
+    private UIElement handleField(IKey label, UITrackpad pad)
+    {
+        UILabel text = UI.label(label, 20);
+
+        text.w(28);
+
+        return UI.row(5, 0, 20, text, pad);
+    }
+
+    /**
+     * Sync the bezier handle trackpads with the keyframe's tangent handle
+     * values, and add or remove the handle rows from the scroll depending on
+     * whether the current keyframe uses BEZIER interpolation (the rows are
+     * physically removed rather than hidden so they don't leave empty gaps).
+     */
+    public void syncBezierHandles()
+    {
+        boolean bezier = this.keyframe.getInterpolation().getInterp() == Interpolations.BEZIER;
+
+        if (bezier != this.bezierHandlesVisible)
+        {
+            this.bezierHandlesVisible = bezier;
+
+            if (bezier)
+            {
+                this.scroll.add(this.lxRow, this.lyRow, this.rxRow, this.ryRow);
+            }
+            else
+            {
+                this.scroll.remove(this.lxRow);
+                this.scroll.remove(this.lyRow);
+                this.scroll.remove(this.rxRow);
+                this.scroll.remove(this.ryRow);
+            }
+
+            this.scroll.resize();
+        }
+
+        if (bezier)
+        {
+            this.lxPad.setValue(this.keyframe.lx);
+            this.lyPad.setValue(this.keyframe.ly);
+            this.rxPad.setValue(this.keyframe.rx);
+            this.ryPad.setValue(this.keyframe.ry);
+        }
     }
 
     public Keyframe<T> getKeyframe()
@@ -199,6 +291,7 @@ public abstract class UIKeyframeFactory <T> extends UIElement
     public void update()
     {
         this.tick.setValue(TimeUtils.toTime(this.keyframe.getTick()));
+        this.syncBezierHandles();
     }
 
     public static interface IUIKeyframeFactoryFactory <T>
